c0c2febfbeb982591a937f06c4a1f4dbc06a0b0d
SpencerHill4 <spencerthefencer10@hotmail.com>
2026-09-25 17:25:22 -0600

    Removed redundant test; menu is handled by orderRouter

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,6 +1,5 @@
 const request = require("supertest");
 const app = require("../service");
-const db = require("../database/database");
 
 const testUser = {
   name: "pizza diner",
@@ -67,26 +66,6 @@ test("logs out an authenticated user", async () => {
 
   expect(ordersRes.status).toBe(401);
   expect(ordersRes.body).toEqual({ message: "unauthorized" });
-});
-
-test("registered user can get a crusty pizza from the menu", async () => {
-  if ((await db.DB.getMenu()).length === 0) {
-    await db.DB.addMenuItem({
-      title: "Crusty",
-      description: "A dry mouthed favorite",
-      image: "pizza4.png",
-      price: 0.0028,
-    });
-  }
-
-  const menuRes = await request(app)
-    .get("/api/order/menu")
-    .set("Authorization", `Bearer ${testUserAuthToken}`);
-
-  expect(menuRes.status).toBe(200);
-  expect(menuRes.body).toEqual(
-    expect.arrayContaining([expect.objectContaining({ title: "Crusty" })]),
-  );
 });
 
 function expectValidJwt(potentialJwt) {
